dba7661dce60543e8dcc38eba70a587b9576ff8d
techstudent2024 <adamsi5412@student.faytechcc.edu>
2024-10-08 14:06:03 -0400

    Update p1hw2_isadeen_adams.py

--- p1hw2_isadeen_adams.py
+++ p1hw2_isadeen_adams.py
@@ -1,1 +1,23 @@
+# CTI 110
+# P1HW2
+# I Adams
+# 10/8/24
+
 print ('This program calculates and displays travel expenses')
+
+# Ask user to enter their budget
+budget = float(input("What is your travel budget? $ "))
+# Where are they going
+destination = input("Where are you going? ")
+# Gas amount?
+gas = int(input("How much on gas? $ "))
+# How much for accommodation/hotel? 
+accommodation = int(input('How much for acommodation/hotel? $ '))
+# Ask amount for food
+food = int (input('How much for food? $ '))
+
+
+
+
+# TODO: the rest
+print("Your budget: $", format(budget,".2f"))
